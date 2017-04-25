1d6fe29c100b7c791583f1602e00a77a5e01f170
HungNguyen <hungng@spc-vn.com>
2017-04-25 14:30:06 +0700

    Add page show list thread

---
+++ wp-content/themes/mugyuu/page-notice-sp.php
@@ -0,0 +1,86 @@
+       <?php get_header(); ?>
+        <div id="sb-site" class="qa wrapper new">
+            <?php breadcrumb(); ?>
+            <section class="newArea qaArea">
+                <h1>質問掲示板</h1>
+                <p class="detail">子育て奮闘中のママさん達が感じた疑問質問やお役立ち情報まで自由に語り合おう♪</p>
+                <div class="btnArea">
+                    <a href="">新規スレッドを立てる</a>
+                </div>
+                <p class="all">スレッド一覧</p>
+                <ul class="articleList newList">
+                    <?php
+                        $posts_per_page = 10;
+                        $val = new WP_Query([
+                            'posts_per_page' => $posts_per_page,
+                        ]);
+                        $query = new WP_Query([
+                            'cat' =>array(-1, -281),
+                            'post_type' => array('thread_post', 'question_post'),
+                            'posts_per_page' => $posts_per_page,
+                            'paged' => ($paged > $val->max_num_pages) ? $val->max_num_pages : $paged,
+                        ]);
+                    ?>
+                    <?php
+                        if ($query -> have_posts()) :
+                        while($query -> have_posts()) : $query -> the_post(); ?>
+                        <?php
+                            $post_cat = get_the_category();
+                                usort( $post_cat , '_usort_terms_by_ID');
+                            $count = count($post_cat);
+                            if($count === 3) {//カテが3
+                                $catNameGrandson = $post_cat[2]->cat_name;
+                            }elseif($count === 2){//カテが2
+                                $catNameGrandson = $post_cat[1]->cat_name;
+                            }else{
+                                $catNameGrandson = $post_cat[0]->cat_name;
+                            }
+							$thumbnail_id = get_post_thumbnail_id();
+							$image = wp_get_attachment_image_src( $thumbnail_id, 'list_thumbnail' );
+                            $author = get_userdata($post->post_author);
+                            $authorRoles = $author->roles;
+                            usort( $authorRoles , '_usort_terms_by_ID');
+                            $author_id = $post->post_author;
+                        ?>
+                    <li>
+                        <a href="<?php the_permalink(); ?>">
+							<div class="imgArea" style="background-image: url(<?php echo $image[0]; ?>);">
+								<img src='data:image/gif;base64,R0lGODlhAQABAIAAAP//////zCH5BAEHAAAALAAAAAABAAEAAAICRAEAOw=='>
+							</div>
+                            <div class="content">
+                                <div class="articleData">
+                                    <p class="data"><?php the_time('Y/m/d'); ?></p>
+                                    <p class="cat"><?php echo $catNameGrandson; ?></p>
+                                </div>
+                                <h2><?php the_title(); ?></h2>
+                                <div class="articleData">
+                                    <?php if($author_id !== '66') { ?>
+                                        <p class="name">
+                                            <?php
+                                                 if  ($authorRoles[0] == 'editor' ) {
+                                                     echo '<span class="icon-mugyuu"></span>';
+                                                 }
+                                            ?>
+                                            <?php the_author(); ?>
+                                        </p>
+                                    <?php } ?>
+                                    <div class="pv">
+                                        <i class="fa fa-comments" aria-hidden="true"></i>
+                                        <?php echo wp_count_comments( get_the_ID() )->total_comments; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <?php endwhile; else: ?>
+                    <p class="none">該当する記事がありません。</p>
+                    <?php endif; ?>
+                </ul>
+                <?php
+                    if(function_exists("pagination")) {
+                        pagination($val->max_num_pages);
+                    }
+                ?>
+                <?php wp_reset_postdata(); ?>
+            </section>
+    <?php get_footer(); ?>
