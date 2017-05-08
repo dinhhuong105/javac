ecbb87afffd0cd25ec4e7cad2aab03c908584dfa
HungNguyen <hungng@spc-vn.com>
2017-05-08 17:59:44 +0700

    Change text for comment on thread post

--- wp-content/themes/mugyuu/notice_comments-sp.php
+++ wp-content/themes/mugyuu/notice_comments-sp.php
@@ -63,7 +63,9 @@
             </li>
             <li>
                 <h3>コメント<span class="red">※</span></h3>
-                <p class="notes">参考になるような意見を書いてね！誹謗中傷コメントは消しちゃうよ！的な注意コメント入れる</p>
+                <p class="notes">ご自身の状況や良かった点、困った点などを具体的に書きましょう！
+                        育児で困ってる方の参考になり共感ボタンをもらいやすくなります！
+                        説明が難しい場合は画像などもあるとわかりやすいです。</p>
                 <div class="textArea" id="contentArea">
                     <textarea name="comment" id="thread_content" required cols="30" rows="10"></textarea>
                     <label class="imgBtn">
