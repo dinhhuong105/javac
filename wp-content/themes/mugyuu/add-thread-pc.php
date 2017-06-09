bdec8ed6ee0291df78fbf9bf997a6637795a0040
HungNguyen <hungng@spc-vn.com>
2017-06-09 17:10:38 +0700

    Add fixPage for add-thread

--- wp-content/themes/mugyuu/add-thread-pc.php
+++ wp-content/themes/mugyuu/add-thread-pc.php
@@ -3,7 +3,7 @@
 		<ul class="breadcrumbList">
 			<li><a href="<?php echo home_url('/'); ?>">トップ</a></li>
 			<li><i class="fa fa-angle-right arrowIcon"></i><a href="<?php echo home_url('/'); ?>notice"><span>質問掲示板</span></a></li>
-			<li><i class="fa fa-angle-right arrowIcon"></i><span>新規スレッド作成</span></li>
+			<li class="fixPage"><i class="fa fa-angle-right arrowIcon"></i><span>新規スレッド作成</span></li>
 		</ul>
 	</div>
     <div class="mainWrap single addthread">
